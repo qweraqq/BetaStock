39dffead9428be710beb0bb28e85eae1c604845b
qweraqq <shenxiangxiang@gmail.com>
2016-03-18 15:03:22 +0800

    add stock normalization

--- Preprocess.py
+++ Preprocess.py
@@ -9,11 +9,36 @@ from pip.utils.ui import hidden_cursor
 from str2vec import *
 from keras.preprocessing import sequence
 from datetime import datetime, date, time, timedelta
-
 import logging
 # logging.basicConfig(level=logging.CRITICAL)
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger('Preprocess.py')
+
+
+def featureNormalization(X, mode=0):
+    """
+    :param X: (nb_examples, nb_features) matrix
+              here, nb_samples a.k.a timesteps
+              X[: ,0] = (open-last_day_close)/last_day_close
+              X[: ,1] = (high-last_day_close)/last_day_close
+              X[: ,2] = (low-last_day_close)/last_day_close
+              X[: ,3] = (close-last_day_close)/last_day_close
+              X[: ,4] = turnover rate
+              X[: ,5] = close
+    :param mode: dummy now
+    :return: normalized X
+    """
+
+    if mode == 1:
+        X[:, 4] = X[:, 4] * 100
+        X[:, 4] = X[:, 4] / 320000000000
+    X[:, 3] = X[:, 3]/100
+    tmp = X[1:, :]
+    last_day_close = X[0:-1, 5]
+    last_day_close = last_day_close.reshape((len(last_day_close), 1))
+    tmp[:, 0:3] = (tmp[:, 0:3] - last_day_close) / last_day_close
+    tmp[:, 0:5] = tmp[:, 0:5] * 10
+    return tmp[:, 0:5]
 
 
 class Preprocessor(object):
@@ -104,8 +129,10 @@ class Preprocessor(object):
         stock_data = ts.get_hist_data('sh', start=self.formatDateString(dt2),
                                       end=self.formatDateString(dt))
 
-        stock_data = stock_data.as_matrix(['open', 'high', 'low', 'p_change', 'volume'])
+        stock_data = stock_data.as_matrix(['open', 'high', 'low', 'p_change', 'volume', 'close'])
         stock_data = stock_data[stock_data.shape[0]::-1, :]
+        stock_data = featureNormalization(stock_data, mode=1)
+        stock_data = stock_data[np.newaxis, :, :]
         return stock_data
 
     def getNextDayStockPchange(self, dt=None, max_td=7):
@@ -156,6 +183,7 @@ class Preprocessor(object):
         base_time = datetime(1991, 12, 20, 0, 0)
         r_news = None
         r_pchange = None
+        r_stock = None
         with open(filename, 'r') as f:
             for line in f.readlines():
                 line = line.strip()
@@ -173,6 +201,8 @@ class Preprocessor(object):
                         one_day_news.append(self.convertOneNewsToVec(news))
                     elif (line_time-base_time).days >= 1 \
                             and len(one_day_news) > 0:
+
+                        # first, r_news
                         tmp = self.convertOneDayAllNewsToVec(one_day_news,
                                                              max_len=max_len)
                         if r_news is None:
@@ -180,6 +210,12 @@ class Preprocessor(object):
                         else:
                             r_news = np.vstack((r_news, tmp))
 
+                        # second, r_stock
+                        tmp = self.getPreviousStockData(dt=base_time-timedelta(days=1),
+                                                        td=300)
+
+
+                        # third, r_pchange
                         if return_pchange:
                             p_change = self.getNextDayStockPchange(base_time-timedelta(days=1))
                             logger.info("next day change ="+str(p_change))
@@ -232,7 +268,8 @@ if __name__ == '__main__':
     # news_rep_list = np.vstack((news_rep1, news_rep2))
     # print p.convertOneDayAllNewsToVec(news_rep_list)
     # print p.convertOneDayAllNewsToVec(news_rep_list).shape
-    #m = p.getPreviousStockData(td=2)
+    m = p.getPreviousStockData(td=3)
+    print m
     X,y = p.readNewsFromFile('201501news.txt', max_len=3)
-    print X
-    print y
+    # print X
+    #print y
