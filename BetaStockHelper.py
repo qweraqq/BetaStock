dc27fee291c27c25574b9298b29cacc9e2aa0b73
qweraqq <shenxiangxiang@gmail.com>
2016-03-26 11:15:25 +0800

    add read local data

--- BetaStockHelper.py
+++ BetaStockHelper.py
@@ -3,6 +3,7 @@ from str2vec import *
 from datetime import datetime, timedelta
 import tushare as ts
 import numpy as np
+import pandas as pd
 
 def formatDateString(dt):
     """
@@ -33,7 +34,7 @@ def featureNormalization(X, mode=0):
               X[: ,2] = (low-last_day_close)/last_day_close
               X[: ,3] = (close-last_day_close)/last_day_close
               X[: ,4] = turnover rate or volume if index(mode1)
-              X[: ,5] = close
+              X[: ,5] = close (not included in return value)
     :param mode: is it index or stock, mode0-stock, mode1-index
     :return: normalized X
     """
@@ -41,8 +42,8 @@ def featureNormalization(X, mode=0):
     if mode == 1:
         X[:, 4] = X[:, 4] * 100
         X[:, 4] = X[:, 4] / 320000000000  # shanghai stock market total value
-    X[:, 3] = X[:, 3]/100
-    tmp = X[1:, :]
+    X[:, 3] = X[:, 3]/100  # p_change
+    tmp = X[1:, :]  # close, remove first day which do not have a previous close
     last_day_close = X[0:-1, 5]
     last_day_close = last_day_close.reshape((len(last_day_close), 1))
     tmp[:, 0:3] = (tmp[:, 0:3] - last_day_close) / last_day_close
@@ -149,11 +150,11 @@ class BetaStockHelper(object):
                                       end=self.formatDateString(dt), retry_count=10)
 
         stock_data = stock_data.as_matrix(['open', 'high', 'low', 'p_change', 'volume', 'close'])
-        stock_data = stock_data[stock_data.shape[0]::-1, :]
+        stock_data = stock_data[stock_data.shape[0]::-1, :]  # reserve
         stock_data = featureNormalization(stock_data, mode=1)
         return stock_data
 
-    def __getStockDate(self, dt):
+    def _getStockDate(self, dt):
         """
 
         :param dt:
@@ -167,14 +168,40 @@ class BetaStockHelper(object):
             dt = datetime(dt.year, dt.month, dt.day, 0, 0) + timedelta(days=1)
         return dt
 
+    def readSingleFromFile(self, filename, mode=0):
+        """
+        :param filename: saved stock file, like '600000.csv'
+        :param mode: mode 0 - regular stocks
+                     mode 1 - index
+        :return: tuple (X ,y)
+                 X = (1,nb_samples,nb_features) 3d tensor
+                 y = (1,nb_samples,1) 3d tensor
+                 here, nb_samples a.k.a timesteps
+        """
+        if mode == 0:
+            df = pd.read_csv(filename, sep=',', header=0, usecols=[1, 2, 4, 7, 14, 3])
+            X = df.as_matrix(['open', 'high', 'low', 'p_change', 'turnover', 'close'])
+
+        if mode == 1:
+            df = pd.read_csv(filename, sep=',', header=0, usecols=[1, 2, 4, 5, 7, 3])
+            X = df.as_matrix(['open', 'high', 'low', 'p_change', 'volume', 'close'])
+        X = X[::-1, :]
+        X = featureNormalization(X, mode=mode)
+        r_X = X[np.newaxis, 0:-1, :]
+        r_y = X[1:, 3]
+        r_y = r_y.reshape((len(r_y),1))
+        r_y = r_y[np.newaxis, :, :]
+        return r_X, r_y
+
 
 if __name__ == '__main__':
     helper = BetaStockHelper()
-    news_rep1 = helper.str2Vec("平安称陆金所下半年启动上市 不受战新板不确定性影响")
-    news_rep2 = helper.str2Vec("财经观察：全球经济风险致美联储暂缓加息")
-    print news_rep1
-    print '--------------------------'
-    print np.sum(news_rep2**2)
+    # news_rep1 = helper.str2Vec("平安称陆金所下半年启动上市 不受战新板不确定性影响")
+    # news_rep2 = helper.str2Vec("财经观察：全球经济风险致美联储暂缓加息")
+    # print news_rep1
+    # print '--------------------------'
+    # print np.sum(news_rep2**2)
+    helper.readSingleFromFile('test.csv', mode=1)
 
 
 
