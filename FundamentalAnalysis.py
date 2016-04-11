549443cb454a27543aff5a63d8550f51f71d6608
qweraqq <shenxiangxiang@gmail.com>
2016-04-11 22:04:43 +0800

    alpha version

--- FundamentalAnalysis.py
+++ FundamentalAnalysis.py
@@ -28,7 +28,7 @@ def custom_objective(y_true, y_pred):
     # weight_matrix = T.exp(T.abs_(y_true-y_pred)/50)
     # T.abs_(y1-y)
     # (y1-y)**2
-    weight_matrix = T.log10(1+T.abs_(y_true))
+    weight_matrix = T.abs_(y_true)
     return T.mean(0.5*weight_matrix*(y_true-y_pred)**2)
 
 
@@ -48,20 +48,20 @@ if __name__ == '__main__':
     nb_hidden = 200
     model = Sequential()
     lstm1 = LSTM(output_dim=nb_hidden, input_dim=data_dim,
-                 dropout_U=0.6, dropout_W=0.6, W_regularizer=l2(0.01), b_regularizer=l2(0.01))
+                 dropout_U=0.3, dropout_W=0.3, W_regularizer=l2(0.001), b_regularizer=l2(0.001))
     lstm2 = LSTM(output_dim=nb_hidden, input_dim=data_dim,
-                 dropout_U=0.6, dropout_W=0.6, W_regularizer=l2(0.01), b_regularizer=l2(0.01))
+                 dropout_U=0.3, dropout_W=0.3, W_regularizer=l2(0.001), b_regularizer=l2(0.001))
     blstm = Bidirectional(forward=lstm1, backward=lstm2)
     blstm.set_input_shape((np.shape(X)[0], timesteps, data_dim))
     model.add(blstm)
-    model.add(Dropout(0.6))
+    model.add(Dropout(0.3))
     model.add(Dense(1, W_regularizer=l2(0.01), b_regularizer=l2(0.01)))
     # model.add(Dropout(0.5))
     # model.add(Dense(nb_classes, input_dim=2*nb_hidden, activation='linear'))
     earlyStopping = keras.callbacks.EarlyStopping(monitor='val_loss',
-                                                  patience=0, verbose=0, mode='auto')
+                                                  patience=10, verbose=0, mode='auto')
     model.compile(loss=custom_objective, optimizer='rmsprop')
-    model.fit(X, y, batch_size=10, nb_epoch=10, validation_data=(X_test, y_test),
+    model.fit(X, y, batch_size=30, nb_epoch=3, validation_data=(X_test, y_test),
               shuffle=True, callbacks=[earlyStopping])
 
     json_string = model.to_json()
